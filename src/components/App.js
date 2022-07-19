9ec6c2ce5f525ec2a2a9a5b558978ad3780cb09f
Seth David Gyimah <esec26@gmail.com>
2022-07-19 17:55:40 +0000

    props destructuring

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,10 @@ function App() {
   const posterURL =
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
   const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
-
+  
   return (
     <div className="App">
-      <MovieCard title={title} genres={genresArr} />
+      <MovieCard title={title} genres={genresArr} posterSrc={posterURL} />
     </div>
   );
 }
